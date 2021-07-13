d24cf9a1b47a33e94381753cc2e384876d563948
Matheus Loose <mwsl.loose@hotmail.com>
2021-07-13 15:26:37 -0300

    RestTamplate

--- hr-payroll/src/main/java/com/loose/hrpayroll/entities/Worker.java
+++ hr-payroll/src/main/java/com/loose/hrpayroll/entities/Worker.java
@@ -1,21 +1,11 @@
-package com.loose.hrworker.entities;
+package com.loose.hrpayroll.entities;
 
 import java.io.Serializable;
-
 import java.util.Objects;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
 
-@Entity
-@Table(name = "tb_worker")
 public class Worker implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+
 	private Long id;
 	private String name;
 	private Double dailyIncome;
--- hr-payroll/src/main/java/com/loose/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/loose/hrpayroll/services/PaymentService.java
@@ -1,13 +1,30 @@
 package com.loose.hrpayroll.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.loose.hrpayroll.entities.Payment;
+import com.loose.hrpayroll.entities.Worker;
 
 @Service
 public class PaymentService {
 
+	@Value("${hr-worker.host}")
+	private String workerHost;
+
+	@Autowired
+	private RestTemplate restTamplate;
+
 	public Payment getPayment(long workerId, int days) {
-		return new Payment("Bob",200.0, days);
+		Map<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("id", "" + workerId);
+
+		Worker worker = restTamplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
