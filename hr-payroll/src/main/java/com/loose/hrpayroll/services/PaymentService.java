477e271d3c16e833712c17cf017893d5b485247d
Matheus Loose <mwsl.loose@hotmail.com>
2021-07-14 10:02:15 -0300

    Created WorkerFeignClient

--- hr-payroll/src/main/java/com/loose/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/loose/hrpayroll/services/PaymentService.java
@@ -1,30 +1,21 @@
 package com.loose.hrpayroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.loose.hrpayroll.entities.Payment;
 import com.loose.hrpayroll.entities.Worker;
+import com.loose.hrpayroll.feignclients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
 
-	@Value("${hr-worker.host}")
-	private String workerHost;
-
 	@Autowired
-	private RestTemplate restTamplate;
+	private WorkerFeignClient workerFeignClient;
 
 	public Payment getPayment(long workerId, int days) {
-		Map<String, String> uriVariables = new HashMap<>();
-		uriVariables.put("id", "" + workerId);
 
-		Worker worker = restTamplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+		Worker worker = workerFeignClient.findById(workerId).getBody();
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
